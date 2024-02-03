8d9d1f8bf00f07d281b160b4508cf8555d954fd4
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-04 02:12:34 +0300

    Implement /category/list route

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,37 @@
-export function categoryList(req, res, next) {
-  res.send('NOT IMPLEMENTED: Category list');
+import createError from 'http-errors';
+import Category from '../models/category.js';
+import Item from '../models/item.js';
+
+export async function categoryList(req, res, next) {
+  const categories = await Category.find()
+    .sort({ name: 1 })
+    .exec()
+    .catch(() => {});
+
+  if (!categories) {
+    const err = createError(500, 'No Database Response');
+    return next(err);
+  }
+
+  const itemCounts = await Promise.all(
+    categories.map((category) =>
+      Item.countDocuments({ category: category.id }).exec()
+    )
+  ).catch(() => {});
+
+  if (!itemCounts) {
+    const err = createError(500, 'No Database Response');
+    return next(err);
+  }
+
+  categories.forEach((category, id) => {
+    category.itemCount = itemCounts[id];
+  });
+
+  res.render('category-list', {
+    title: 'Product Categories',
+    categories
+  });
 }
 
 export function categoryDetails(req, res, next) {
